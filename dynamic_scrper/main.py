7afa5b73e6acc4ce7c34b5385c83f61eec0666f3
jangth0056 <jangth0056@naver.com>
2024-01-14 23:06:01 +0900

    24-01-14-3

--- dynamic_scrper/main.py
+++ dynamic_scrper/main.py
@@ -9,7 +9,7 @@ browser = p.chromium.launch(headless=False) #기본값 True
 
 page = browser.new_page()
 
-page.goto('https://www.wanted.co.kr/jobsfeed?utm_source=google&utm_medium=sa&utm_campaign=kr_recruit_web_sa_signup&utm_term=wanted&utm_content=brand_new&gad_source=1&gclid=CjwKCAiAqY6tBhAtEiwAHeRopS3-vUc2AC-SpkXbsoM59_b4EcGlUlwIi0TIYqQ8WBnCgjNEYbBJ0RoCjlsQAvD_BwE')
+page.goto('https://www.wanted.co.kr')
 time.sleep(3)
 
 page.click("button.Aside_searchButton__Xhqq3")
@@ -24,14 +24,32 @@ time.sleep(3)
 
 page.click("a#search_tab_position")
 time.sleep(3)
-for x in range(5):
-    time.sleep(3)
+for x in range(8):
+    time.sleep(2)
     page.keyboard.down("End")
 
 content = page.content()
 p.stop()
 
 soup = BeautifulSoup(content, "html.parser")
+jobs = soup.find_all("div", class_="JobCard_container__FqChn")
+
+jobs_db = []
+for job in jobs:
+    link = f'https://www.wanted.co.kr{job.find("a")["href"]}' #data-position-name 가능
+    title = job.find("strong", class_="JobCard_title__ddkwM").text
+    company = job.find("span", class_="JobCard_companyName__vZMqJ").text
+    location = job.find("span", class_="JobCard_location__2EOr5").text
+    reward = job.find("span", class_="JobCard_reward__sdyHn").text
+    job = {
+        "title": title,
+        "company": company,
+        "location": location,
+        "reward": reward,
+        "link": link
+    }
+    jobs_db.append(job)
+print(jobs_db)
 
     # page.keyboard.down("End")
     # time.sleep(3)
